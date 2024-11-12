b3f9e8069548854616a60fc83a1680e7d6aa3224
wisher-18 <johnnysonn10@gmail.com>
2024-11-13 00:25:00 +0530

    Checkout, Order Page, Payment Intigration Completed

--- app/Livewire/CheckoutPage.php
+++ app/Livewire/CheckoutPage.php
@@ -3,8 +3,14 @@
 namespace App\Livewire;
 
 use App\Helpers\CartManagement;
+use App\Mail\OrderPlaced;
+use App\Models\Address;
+use App\Models\Order;
+use Illuminate\Support\Facades\Mail;
 use Livewire\Attributes\Title;
 use Livewire\Component;
+use Stripe\Checkout\Session;
+use Stripe\Stripe;
 
 #[Title('Checkout')]
 class CheckoutPage extends Component
@@ -18,6 +24,13 @@ class CheckoutPage extends Component
     public $zip_code;
     public $payment_method;
 
+    public function mount(){
+        $cart_items = CartManagement::getCartItemsFromCookie();
+        if(count($cart_items) == 0){
+            return redirect('/products');
+        }
+    }
+
     public function placeOrder(){
         $this->validate([
             'first_name' => 'required',
@@ -29,6 +42,73 @@ class CheckoutPage extends Component
             'zip_code' => 'required',
             'payment_method' => 'required'
         ]);
+
+        $cart_items = CartManagement::getCartItemsFromCookie();
+
+        $line_items = [];
+
+        foreach($cart_items as $item){
+            $line_items[] = [
+                'price_data' => [
+                    'currency' => 'inr',
+                    'product_data' => [
+                        'name' => $item['name'],
+                    ],
+                    'unit_amount' => $item['unit_amount'] * 100, // Amount in cents/paise
+                ],
+                'quantity' => $item['quantity'],
+            ];
+        }
+
+
+        $order = new Order();
+        $order->user_id = auth()->user()->id;
+        $order->grand_total = CartManagement::calculateGrandTotal($cart_items);
+        $order->payment_method = $this->payment_method;
+        $order->payment_status = 'pending';
+        $order->status = 'new';
+        $order->currency = 'inr';
+        $order->shipping_amount = 0;
+        $order->shipping_method = 'none';
+        $order->notes = 'Order placed by '.auth()->user()->name;
+
+        $address = new Address();
+        $address->first_name = $this->first_name;
+        $address->last_name = $this->last_name;
+        $address->phone = $this->phone;
+        $address->street_address = $this->street_address;
+        $address->city = $this->city;
+        $address->state = $this->state;
+        $address->zip_code = $this->zip_code;
+
+        $redirect_url = '';
+
+        if($this->payment_method == 'stripe'){
+            Stripe::setApiKey(env('STRIPE_SECRET'));
+            $sessionCheckout = Session::create([
+                'payment_method_types' => ['card'],
+                'customer_email' => auth()->user()->email,
+                'line_items' => $line_items,
+                'mode' => 'payment',
+                'success_url' => route('success').'?session_id={CHECKOUT_SESSION_ID}',
+                'cancel_url' => route('cancel'),
+            ]);
+
+            $redirect_url = $sessionCheckout->url;
+
+        }else{
+            $redirect_url = route('success');
+        }
+
+        $order->save();
+        $address->order_id = $order->id;
+        $address->save();
+
+        $order->items()->createMany($cart_items);
+        CartManagement::clearCartItems($cart_items);
+        Mail::to(request()->user())->send(new OrderPlaced($order));
+        return redirect($redirect_url);
+
     }
 
     public function render()
--- resources/views/livewire/checkout-page.blade.php
+++ resources/views/livewire/checkout-page.blade.php
@@ -93,7 +93,7 @@
                     </div>
                     <ul class="grid w-full gap-6 md:grid-cols-2">
                         <li>
-                            <input wire:model='payment_method' class="hidden peer" id="hosting-small" required="" type="radio" value="hosting-small" />
+                            <input wire:model='payment_method' class="hidden peer" id="hosting-small" required="" type="radio" value="cod" />
                             <label class="inline-flex items-center justify-between w-full p-5 text-gray-500 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-700 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-600 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700" for="hosting-small">
                                 <div class="block">
                                     <div class="w-full text-lg font-semibold">
@@ -169,7 +169,8 @@
                     </hr>
                 </div>
                 <button type='submit' class="w-full p-3 mt-4 text-lg text-white bg-green-500 rounded-lg hover:bg-green-600">
-                    Place Order
+                    <span wire:loading.remove>Place Order</span>
+                    <span wire:loading>Processing...</span>
                 </button>
                 <div class="p-4 mt-4 bg-white shadow rounded-xl sm:p-7 dark:bg-slate-900">
                     <div class="mb-2 text-xl font-bold text-gray-700 underline dark:text-white">
